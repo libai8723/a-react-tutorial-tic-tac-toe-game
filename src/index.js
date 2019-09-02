c0a13f22cc9483c0e7de3b01556b31342caf3ad4
libai8723 <libai8723@qq.com>
2019-09-02 13:25:30 +0800

    lift states data to Board to manage passing value and function to every square.

--- src/index.js
+++ src/index.js
@@ -3,28 +3,46 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
     render() {
         return (
             <button
                 className="square"
-                onClick={() => this.setState({ value: 'X' })}
+                // here is something slightly different with tutorials
+                // in the tutorials they use another arrow function to encapsulate this.props.onClick()
+                // but i don't think that is necessary, 
+                // because onClick on expect a function, and this.props.onClick is already a function
+                // why bother to encapsulate it again? And this works
+                onClick={this.props.onClick}
             >
-                {this.state.value}
+                {this.props.value}
             </button>
         );
     }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        }
+    }
+
     renderSquare(i) {
-        return <Square value={i} />;
+        return(
+            <Square 
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
+    }
+
+    handleClick(i) {
+        // get an copy of squares in state
+        const squares = this.state.squares.slice();
+        
+        squares[i] = 'X';
+        this.setState({squares: squares});
     }
 
     render() {
