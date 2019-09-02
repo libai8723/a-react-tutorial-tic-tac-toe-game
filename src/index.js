0e9a512e5b7b0b28bf45c44f030c8a9ed983a0e2
libai8723 <libai8723@qq.com>
2019-09-02 19:27:36 +0800

    Time Travel

--- src/index.js
+++ src/index.js
@@ -65,11 +65,19 @@ class Game extends React.Component {
                 squares: Array(9).fill(null),
               }],
               xIsNext: true,
+              stepNumber: 0,
         }
     }
 
+    jumpTo(step) {
+        this.setState({
+          stepNumber: step,
+          xIsNext: (step % 2) === 0,
+        });
+    }
+
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         // get an copy of squares in state
         const squares = current.squares.slice();
@@ -83,12 +91,13 @@ class Game extends React.Component {
                     [{squares: squares}]
                 ),
             xIsNext: !this.state.xIsNext,
+            stepNumber: history.length,
         });
     }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
         const moves = history.map((step, move) => {
@@ -97,7 +106,7 @@ class Game extends React.Component {
               'Go to game start';
             return (
               <li key={move}>
-                <button>{desc}</button>
+                <button onClick={() => this.jumpTo(move)}>{desc}</button>
               </li>
             );
           });
