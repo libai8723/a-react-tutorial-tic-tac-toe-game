61c843a864d05c46e8efc3b25b6a9f55298caf53
libai8723 <libai8723@qq.com>
2019-09-04 13:17:25 +0800

    use two loops to init the board

--- src/index.js
+++ src/index.js
@@ -34,25 +34,21 @@ class Board extends React.Component {
         );
     }
 
+    makeSquares() {
+        let board = []
+        for (let row = 0; row < 3; row++) {
+            let row_cell = [];
+            for (let col = 0; col < 3; col++) {
+                row_cell.push(this.renderSquare(row * 3 + col))
+            }
+            board.push(<div className="board-row">{row_cell}</div>)
+        }
+        return <div>{board}</div>
+    }
+
     render() {
         return (
-            <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
+            this.makeSquares()
         );
     }
 }
